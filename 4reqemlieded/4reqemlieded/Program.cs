d299ebb27a248dfc2ce05ef49e35a7036c9d6b07
ashraf032 <101041982+ashraf032@users.noreply.github.com>
2022-03-25 19:12:47 +0400

    Result commit

---
+++ 4reqemlieded/4reqemlieded/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _4reqemlieded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string b = "4";
+
+            string a = "44";
+
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("Zehmet olmasa 3 reqemli eded daxil edin");
+
+            string num = Console.ReadLine();
+
+            string couplestr = b + num + a;
+
+            Console.WriteLine(couplestr);
+
+            int resultparam = int.Parse(couplestr);
+
+            if (resultparam >=1000)
+            {
+                Console.WriteLine(resultparam * 0.44);
+            }
+            else
+            {
+                Console.WriteLine("Nezere alinmayib");
+            }
+        }
+    }
+}
